82aa8d898ef8029c0905a5327b847de25724db6a
suri312006 <suri312006@gmail.com>
2024-08-20 15:04:31 -0700

    done with first

---
+++ rs/src/linkedlist/second.rs
@@ -0,0 +1,78 @@
+use std::mem;
+pub struct List {
+    head: Link,
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+type Link = Option<Box<Node>>;
+
+impl List {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            // nah this is wild
+            next: mem::replace(&mut self.head, None),
+        });
+
+        self.head = Some(new_node)
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, None) {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut curr_link = mem::replace(&mut self.head, None);
+
+        while let Some(mut boxed_node) = curr_link {
+            curr_link = mem::replace(&mut boxed_node.next, None);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
